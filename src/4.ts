480e34f8ae922efa212df64490304e07c29f343f
Anastasia-Filipenko <filipenko.nastyha@gmail.com>
2024-04-11 16:21:26 +0300

    add hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,38 @@
+class Key {
+  private signature: number = Math.random();
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  public door: boolean = true;
+  protected tenants: Person[];
+  constructor(protected key: Key) {}
+  abstract openDoor(key: Key): void;
+  comeIn(tenant: Person) {
+    if (this.door) {
+      this.tenants.push(tenant);
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log(`Door open`);
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +41,4 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
-
 export {};
